fdd4a33507058e444b3f017bb76e09fea4a4c5cd
nuwagalb <isnuwa@gmail.com>
2020-09-17 07:53:33 +0300

    make navigations drop down automatically

--- _/js/myscript.js
+++ _/js/myscript.js
@@ -1,5 +1,14 @@
-
-
+$(function() {
+  //make menus drop automatically
+  $("ul.nav li.dropdown").hover(
+    function() {
+      $(".dropdown-menu", this).fadeIn();
+    },
+    function() {
+      $(".dropdown-menu", this).fadeOut("fast");
+    }
+  ); //hover
+}); //executed when jquery is loaded
 
 
 
